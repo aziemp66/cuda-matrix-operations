6bd54eda12b17b9c8a82da6ab7a5fcd80d072014
Azie Melza Pratama <aziemp66@gmail.com>
2025-11-10 00:05:52 +0700

    feat : add config.cpp

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -1,8 +1,8 @@
 #include "config.h"
 
 namespace DefaultConfig {
-    const int DEFAULT_TPB = 32;
-    const int DEFAULT_SIZE = 1024;
-    const char* DEFAULT_CPU_LOG_FILE = "cpu_results.csv";
-    const char* DEFAULT_GPU_LOG_FILE = "gpu_results.csv";
-}
+const int DEFAULT_TPB = 32;
+const int DEFAULT_SIZE = 1024;
+const char *DEFAULT_CPU_LOG_FILE = "cpu_results.csv";
+const char *DEFAULT_GPU_LOG_FILE = "gpu_results.csv";
+} // namespace DefaultConfig
